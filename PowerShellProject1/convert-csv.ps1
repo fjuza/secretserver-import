e8966c014cc2994343a488a31c282710e50ee9ff
Freddie Juza <freddie.juza@syspartner.se>
2015-10-01 13:53:05 +0200

    convert-csv.ps1: * Fixed issue with folders output. * Added parameters * Added help * Fixed some basic logic TODO: Rewrite and use a filestream instead. ATM very slow with large files. convert-toxml.ps1 * Added parameters * Added help * Added logic for some SecretTemplates * Corrected faulty xml output. TODO: Write better logic for SecretServerTemplates. Beautify the code.

--- PowerShellProject1/convert-csv.ps1
+++ PowerShellProject1/convert-csv.ps1
@@ -1,36 +1,48 @@
-#
-# convert_csv.ps1
-#
-$rows = Import-Csv 'C:\users\frejuz\Documents\Scripts\Password export_import SecretServer\FakeNPMdump.csv' -Delimiter ';'
+<#
+.SYNOPSIS
+Converts network password manager export to readable CSV files. Outputs two files, folders.csv and secrets.csv.
+.Description
+Converts network password manager export to two readable CSV files, for management and later export to XML
 
-$folders = 'C:\users\frejuz\Documents\Scripts\Password export_import SecretServer\folders.csv'
-$secrets = 'C:\users\frejuz\Documents\Scripts\Password export_import SecretServer\secrets.csv'
+.Parameter -Path
+Location of Network Password Manager output file.
+.Parameter -PathFolder
+Destination path for folder csv.
+.Parameter -PathSecret
+Destination path for secrets csv.
+.EXAMPLE
+.\convert-csv.ps1 -Path c:\temp\export.csv -PathFolder c:\temp\folders.csv -PathSecret c:\secure\secrets.csv
+#>
+param(
+	[parameter(Mandatory=$False)][string]$Path = 'C:\Users\frejuz\Documents\Scripts\Password export_import SecretServer\FakeNPMdump.csv',
+	[parameter(Mandatory=$False)][string]$PathFolder = 'C:\Users\frejuz\Documents\Scripts\Password export_import SecretServer\folders.csv',
+	[parameter(Mandatory=$False)][string]$PathSecret = 'C:\Users\frejuz\Documents\Scripts\Password export_import SecretServer\secrets.csv'
+)
+$rows = Import-Csv $Path -Delimiter ';'
+
+$folders = $PathFolder
+$secrets = $PathSecret
 $secretCollection = @()
 $pathCollection = @()
+Measure-Command{
 $rows | foreach {
 	$name = $PSItem.Name
 	$login = $PSItem.Login
 	$password = $PSItem.Password
 	$url = $PSItem.Url
 	$comment = $PSItem.Comment
-	
 	if($name -like "*\*"){
-		#Write-Host "test"
-		#$name
 		$path = $name
 		$path = $path.Substring(1)
 		$path = $path -replace ".$"
-		$pathName = $name.split("\")
+		$pathName = $path.split("\")
 		$pHash = @{
 			FolderName = $pathName[-1]
-			FolderPath  = $name
-
+			FolderPath  = $path
 		}
 		$pObject = New-Object psobject -Property $pHash
 		$pathCollection += $pObject
 	}
-
-
 	if($name[0] -notlike "*\*"){
 		$hash = $null
 		$object = $null
@@ -38,8 +50,10 @@ $rows | foreach {
 			$comment = $comment.replace('#13','##BR##')
 		}
 		$hash = @{
+			Type = ""
 			FolderPath = $path
 			SecretTemplateName = ""
+			Machine = ""
 			Name = $name
 			Login = $login
 			Password = $password
@@ -48,9 +62,8 @@ $rows | foreach {
 		}
 		$object = New-Object psobject -Property $hash
 		$secretCollection += $object
-		
 	}
-
 }
 $secretCollection | Export-Csv -Delimiter ';' -Path $secrets -NoTypeInformation
 $pathCollection | Export-Csv -Delimiter ';' -Path $folders -NoTypeInformation
+}
--- PowerShellProject1/convert-toxml.ps1
+++ PowerShellProject1/convert-toxml.ps1
@@ -1,62 +1,229 @@
-﻿$folders = import-csv 'C:\users\frejuz\Documents\Scripts\Password export_import SecretServer\folders.csv' -Delimiter ';'
-$secrets = import-csv 'C:\users\frejuz\Documents\Scripts\Password export_import SecretServer\secrets.csv' -Delimiter ';'
+﻿<#
+.SYNOPSIS
+Generates an XML file for import in SecretServer.
+.Description
+Generates an XML file for import in SecretServer.
+.Parameter -Target
+Where to save the xml-file
+.Parameter -PathFolder
+Target path for folder csv.
+.Parameter -PathSecret
+Target path for secrets csv.
+.EXAMPLE
+.\convert-toxml.ps1 -Target c:\temp\export.csv -PathFolder c:\temp\folders.csv -PathSecret c:\secure\secrets.csv
+#>
+param(
+	[parameter(Mandatory=$False)][string]$Target = 'C:\Users\frejuz\Documents\Scripts\Password export_import SecretServer\secretserver.xml',
+	[parameter(Mandatory=$False)][string]$PathFolder = 'C:\Users\frejuz\Documents\Scripts\Password export_import SecretServer\folders.csv',
+	[parameter(Mandatory=$False)][string]$PathSecret = 'C:\Users\frejuz\Documents\Scripts\Password export_import SecretServer\secrets.csv'
+)
 
+$folders = import-csv $PathFolder -Delimiter ';'
+$secrets = import-csv $PathSecret -Delimiter ';'
 
-$path = & 'C:\users\frejuz\documents\scripts\Password export_import SecretServer\test1.xml'
-$xmlWriter = New-Object System.XML.XmlTextWriter($path,$null)
+$xmlWriter = New-Object System.XML.XmlTextWriter($Target,$null)
 
 $xmlWriter.Formatting = 'Indented'
 $xmlWriter.Indentation = 3
-$xmlWriter.BaseStreamIndentChar = " "
-
+$xmlWriter.IndentChar = " "
 $xmlWriter.WriteStartDocument()
-
-$xmlWriter.WriteProcessingInstruction("xml","version='1.0' encoding='UTF-16'")
 $xmlWriter.WriteStartElement('ImportFile')
  $xmlWriter.WriteAttributeString("xmlns:xsi", "http://www.w3.org/2001/XMLSchema-instance")
  $xmlWriter.WriteAttributeString("xmlns.xsd", "http://www.w3.org/2001/XMLSchema")
 $xmlWriter.WriteStartelement("Folders")
 $folders | foreach {
-	$isRootFolder = $PSItem.split('\').length -eq 1 
+	$isRootFolder = $PSItem.ToString().split('\').length -eq 1 
 
 	$xmlWriter.WriteStartElement("Folder")
-	 $xmlWriter.WriteElementString("FolderName", $PSItem.FolderName)
-	 $xmlWriter.WriteElementString("FolderPath", $PSItem.FolderPath)
-	 $xmlWriter.WriteStartElement("Permissions")
+	$xmlWriter.WriteElementString("FolderName", $PSItem.FolderName)
+	$xmlWriter.WriteElementString("FolderPath", $PSItem.FolderPath)
+	$xmlWriter.WriteStartElement("Permissions")
 	if($isRootFolder){
-	  $xmlWriter.WriteStartElement("Permission")
-	   $xmlWriter.WriteElementString("View", "true")
-	   $xmlWriter.WriteElementString("edit", "true")
-	   $xmlWriter.WriteElementString("owner", "true")
-	   $xmlWriter.WriteElementString("UserName", "SS_Administrator")
-	  $xmlWriter.WriteEndElement()
-	  $xmlWriter.WriteStartElement("Permission")
-	   $xmlWriter.WriteElementString("View", "true")
-	   $xmlWriter.WriteElementString("edit", "true")
-	   $xmlWriter.WriteElementString("owner", "false")
-	   $xmlWriter.WriteElementString("GroupName", "Everyone")
-	  $xmlWriter.WriteEndElement()
+		$xmlWriter.WriteStartElement("Permission")
+		$xmlWriter.WriteElementString("View", "true")
+		$xmlWriter.WriteElementString("Edit", "true")
+		$xmlWriter.WriteElementString("Owner", "true")
+		$xmlWriter.WriteElementString("UserName", "SS_Administrator")
+		$xmlWriter.WriteEndElement()
+		$xmlWriter.WriteStartElement("Permission")
+		$xmlWriter.WriteElementString("View", "true")
+		$xmlWriter.WriteElementString("Edit", "true")
+		$xmlWriter.WriteElementString("Owner", "true")
+		$xmlWriter.WriteElementString("GroupName", "SysPartner.local\FS_Ekonomi")
+		$xmlWriter.WriteEndElement()
+		$xmlWriter.WriteStartElement("Permission")
+		$xmlWriter.WriteElementString("View", "true")
+		$xmlWriter.WriteElementString("Edit", "true")
+		$xmlWriter.WriteElementString("Owner", "false")
+		$xmlWriter.WriteElementString("GroupName", "Everyone")
+		$xmlWriter.WriteEndElement()
 	}
-	 $xmlWriter.WriteEndElement()
+	$xmlWriter.WriteEndElement()
 	$xmlWriter.WriteEndElement()
 }
+$xmlWriter.WriteEndElement()
    $xmlWriter.WriteStartElement("Groups")
    $xmlWriter.WriteEndElement()
    $xmlWriter.WriteStartElement("SecretTemplates")
    $xmlWriter.WriteEndElement()
    $xmlWriter.WriteStartElement("Secrets")
     $secrets | foreach {
-		$xmlWriter.WriteElementString("SecretName", $PSItem.SecretName)
+		$Machine = $PSItem.SecretName
+		$Server = ($PSItem.FolderPath).split('\')
+		$Username = $PSItem.login
+		$Password = $PSItem.password
+		$Notes = $PSItem.comment
+		$url = $PSItem.url
+
+		$xmlWriter.WriteStartElement("Secret")
+		$xmlWriter.WriteElementString("SecretName", $PSItem.Name)
 		$xmlWriter.WriteElementString("SecretTemplateName", $PSItem.SecretTemplateName)
 		$xmlWriter.WriteElementString("FolderPath", $PSItem.FolderPath)
 		$xmlWriter.WriteStartElement("Permissions")
 		$xmlWriter.WriteEndElement()
 		$xmlWriter.WriteStartElement("SecretItems")
-		 $xmlWriter.WriteElementString("FieldName", "URL")
-		 $xmlWriter.WriteElementString("Value", $PSItem.url)
+		switch($PSItem.SecretTemplateName){
+			"Windows Account" {
+								$xmlWriter.WriteStartElement("SecretItem")
+								$xmlWriter.WriteElementString("FieldName", "Machine")
+								$xmlWriter.WriteElementString("Value", $Server[-1])
+								$xmlWriter.WriteEndElement()
+								$xmlWriter.WriteStartElement("SecretItem")
+								$xmlWriter.WriteElementString("FieldName", "Username")
+								$xmlWriter.WriteElementString("Value", $Username)
+								$xmlWriter.WriteEndElement()
+								$xmlWriter.WriteStartElement("SecretItem")
+								$xmlWriter.WriteElementString("FieldName", "Password")
+								$xmlWriter.WriteElementString("Value", $Password)
+								$xmlWriter.WriteEndElement()
+								$xmlWriter.WriteStartElement("SecretItem")
+								$xmlWriter.WriteElementString("FieldName", "Notes")
+								$xmlWriter.WriteElementString("Value", $Notes)
+								$xmlWriter.WriteEndElement()
+			}
+			"Web Password" {
+								$xmlWriter.WriteStartElement("SecretItem")
+								$xmlWriter.WriteElementString("FieldName", "URL")
+								if($url -like ''){
+									$xmlWriter.WriteElementString("Value", $Server[-1])
+								} else {
+									$xmlWriter.WriteElementString("Value", $Url)
+								}
+								$xmlWriter.WriteEndElement()
+								$xmlWriter.WriteStartElement("SecretItem")
+								$xmlWriter.WriteElementString("FieldName", "UserName")
+								$xmlWriter.WriteElementString("Value", $Username)
+								$xmlWriter.WriteEndElement()
+								$xmlWriter.WriteStartElement("SecretItem")
+								$xmlWriter.WriteElementString("FieldName", "Password")
+								$xmlWriter.WriteElementString("Value", $Password)
+								$xmlWriter.WriteEndElement()
+								$xmlWriter.WriteStartElement("SecretItem")
+								$xmlWriter.WriteElementString("FieldName", "Notes")
+								$xmlWriter.WriteElementString("Value", $Notes)
+								$xmlWriter.WriteEndElement()
+			}
+			"HP iLO Account (SSH)" {
+								$xmlWriter.WriteStartElement("SecretItem")
+								$xmlWriter.WriteElementString("FieldName", "IP Address / Host Name")
+								$xmlWriter.WriteElementString("Value", $url)
+								$xmlWriter.WriteEndElement()
+								$xmlWriter.WriteStartElement("SecretItem")
+								$xmlWriter.WriteElementString("FieldName", "Username")
+								$xmlWriter.WriteElementString("Value", $Username)
+								$xmlWriter.WriteEndElement()
+								$xmlWriter.WriteStartElement("SecretItem")
+								$xmlWriter.WriteElementString("FieldName", "Password")
+								$xmlWriter.WriteElementString("Value", $Password)
+								$xmlWriter.WriteEndElement()
+								$xmlWriter.WriteStartElement("SecretItem")
+								$xmlWriter.WriteElementString("FieldName", "Notes")
+								$xmlWriter.WriteElementString("Value", $Notes)
+								$xmlWriter.WriteEndElement()
+			}
+			"MySql Account" {
+								$xmlWriter.WriteStartElement("SecretItem")
+								$xmlWriter.WriteElementString("FieldName", "Server")
+								$xmlWriter.WriteElementString("Value", $Server[-1])
+								$xmlWriter.WriteEndElement()
+								$xmlWriter.WriteStartElement("SecretItem")
+								$xmlWriter.WriteElementString("FieldName", "Username")
+								$xmlWriter.WriteElementString("Value", $Username)
+								$xmlWriter.WriteEndElement()
+								$xmlWriter.WriteStartElement("SecretItem")
+								$xmlWriter.WriteElementString("FieldName", "Password")
+								$xmlWriter.WriteElementString("Value", $Password)
+								$xmlWriter.WriteEndElement()
+								$xmlWriter.WriteStartElement("SecretItem")
+								$xmlWriter.WriteElementString("FieldName", "Notes")
+								$xmlWriter.WriteElementString("Value", $Notes)
+								$xmlWriter.WriteEndElement()
+			}
+			"Unix Root Account (SSH)" {
+								$xmlWriter.WriteStartElement("SecretItem")
+								$xmlWriter.WriteElementString("FieldName", "Machine")
+								$xmlWriter.WriteElementString("Value", $Server[-1])
+								$xmlWriter.WriteEndElement()
+								$xmlWriter.WriteStartElement("SecretItem")
+								$xmlWriter.WriteElementString("FieldName", "Username")
+								$xmlWriter.WriteElementString("Value", $Username)
+								$xmlWriter.WriteEndElement()
+								$xmlWriter.WriteStartElement("SecretItem")
+								$xmlWriter.WriteElementString("FieldName", "Password")
+								$xmlWriter.WriteElementString("Value",$Password)
+								$xmlWriter.WriteEndElement()
+								$xmlWriter.WriteStartElement("SecretItem")
+								$xmlWriter.WriteElementString("FieldName", "Notes")
+								$xmlWriter.WriteElementString("Value", $Notes)
+								$xmlWriter.WriteEndElement()
+			}
+			"SQL Server Account" {
+								$xmlWriter.WriteStartElement("SecretItem")
+								$xmlWriter.WriteElementString("FieldName", "Server")
+								$xmlWriter.WriteElementString("Value", $Server[-1])
+								$xmlWriter.WriteEndElement()
+								$xmlWriter.WriteStartElement("SecretItem")
+								$xmlWriter.WriteElementString("FieldName", "Username")
+								$xmlWriter.WriteElementString("Value", $Username)
+								$xmlWriter.WriteEndElement()
+								$xmlWriter.WriteStartElement("SecretItem")
+								$xmlWriter.WriteElementString("FieldName", "Password")
+								$xmlWriter.WriteElementString("Value", $Password)
+								$xmlWriter.WriteEndElement()
+								$xmlWriter.WriteStartElement("SecretItem")
+								$xmlWriter.WriteElementString("FieldName", "Notes")
+								$xmlWriter.WriteElementString("Value", $Notes)
+								$xmlWriter.WriteEndElement()
+			}
+			"AD Service Account" {
+								$xmlWriter.WriteStartElement("SecretItem")
+								$xmlWriter.WriteElementString("FieldName", "Domain")
+								$xmlWriter.WriteElementString("Value", "SP")
+								$xmlWriter.WriteEndElement()
+								$xmlWriter.WriteStartElement("SecretItem")
+								$xmlWriter.WriteElementString("FieldName", "Username")
+								$xmlWriter.WriteElementString("Value", $Username)
+								$xmlWriter.WriteEndElement()
+								$xmlWriter.WriteStartElement("SecretItem")
+								$xmlWriter.WriteElementString("FieldName", "Password")
+								$xmlWriter.WriteElementString("Value", $Password)
+								$xmlWriter.WriteEndElement()
+								$xmlWriter.WriteStartElement("SecretItem")
+								$xmlWriter.WriteElementString("FieldName", "Notes")
+								$xmlWriter.WriteElementString("Value", $Notes)
+								$xmlWriter.WriteEndElement()
+			}
+		}
+		$xmlWriter.WriteEndElement()
 		$xmlWriter.WriteEndElement()
 
 	}
    $xmlWriter.WriteEndElement()
 $xmlWriter.WriteEndElement()
 $xmlWriter.WriteEndElement()
+
+$xmlWriter.WriteEndDocument()
+$xmlWriter.Flush()
+$xmlWriter.Close()
+
+notepad $path
